19a4e26adfc7fe1736fed8cfc773e66a0702c9b3
dinesh <dinesh.molakalapalli@gmail.com>
2021-02-02 19:41:25 -0600

    updated testcase file

--- src/Tester.java
+++ src/Tester.java
@@ -4,6 +4,8 @@ public class Tester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hi, welcome");
+
+System.out.println("My name is Dinesh");
 	}
 
 }
